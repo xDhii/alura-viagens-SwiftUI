6c83febb4e6ec16915d32be137809f5c407d3b9b
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-02-27 22:23:20 -0300

    Initial Commit

---
+++ AluraViagensSwiftUI/AluraViagensSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AluraViagensSwiftUIApp.swift
+//  AluraViagensSwiftUI
+//
+//  Created by Adriano Valumin on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AluraViagensSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
